db191caf998a652fe97e33a5ea6215c5bcdffd17
Koray Erdem <developerkoray@gmail.com>
2021-05-28 00:21:01 +0300

    LifeCycle first exercise is done

--- src/App.js
+++ src/App.js
@@ -1,44 +1,23 @@
-import React, {useState} from 'react';
-import {SafeAreaView, Text, FlatList, Switch, View} from 'react-native';
-
-const data = [
-  {id: 0, name: 'cafe.exe', isFavorite: true},
-  {id: 1, name: 'KafaKafe', isFavorite: false},
-  {id: 2, name: 'BugG', isFavorite: false},
-  {id: 3, name: 'Rock`n Code', isFavorite: true},
-  {id: 4, name: 'do(drink', isFavorite: false},
-  {id: 5, name: 'esc', isFavorite: false},
-];
+import React, {useState, useEffect} from 'react';
+import {Button, SafeAreaView, Text} from 'react-native';
 
 const App = () => {
-  const [cafeList, setCafeList] = useState(data); //uygulama ilk açıldığında bütün kafeleri görmemiz için {data} ekleriz.
-  const [showOnlyFavorites, setShowOnlyFavorites] = useState(false);
+  const [number, setNumber] = useState(0);
+  const [counter, setCounter] = useState(100);
 
-  function onFavoritesChange(isFavoriteSelected) {
-    setShowOnlyFavorites(isFavoriteSelected); //bu çalışmada kilit kısım burası. Burayı anlayınca diğerleri de çorap söküğü gibi geliyor. 
-    isFavoriteSelected
-      ? setCafeList(cafeList.filter(cafe => cafe.isFavorite))
-      : setCafeList(data);
-  }
+useEffect(() => { 
+console.log("number updated");
+}, [number]);
+
+useEffect(() => { 
+  console.log("count updated");
+  }, [counter]);
+
   return (
     <SafeAreaView>
-      <View style={{margin: 10}}>
-        <Text>Show Only Favorites</Text>
-        <Switch
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignSelf: 'flex-start',
-            marginTop: 10,
-          }}
-          value={showOnlyFavorites}
-          onValueChange={onFavoritesChange}
-        />
-      </View>
-      <FlatList
-        data={cafeList}
-        renderItem={({item}) => <Text style={{fontSize: 30}}>{item.name}</Text>}
-      />
+      <Text>Hello Lifecycle</Text>
+      <Button title="Up!" onPress={() => setNumber(number + 1)} />
+      <Button title="CountUp!" onPress={() => setCounter(counter + 100)} />
     </SafeAreaView>
   );
 };
