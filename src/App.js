b58d0393eb46cb9c84cfd56a94002c8a8109b7d3
Koray Erdem <developerkoray@gmail.com>
2021-05-27 22:32:40 +0300

    button click to count up & down practice

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, Text, Button} from 'react-native';
 
 const App = () => {
+const [counter, setCounter] = useState(0);
+
+function increaseCounter() {
+   setCounter(counter + 1);
+}
+
+function decreaseCounter() {
+  setCounter(counter - 1);
+}
+
   return (
     <SafeAreaView>
-      <Text>Hello State!</Text>
+      <Text style={{fontSize:40}}>Counter: {counter}</Text>
+      <Button title="Increase Counter" onPress={increaseCounter} />
+      <Button title="Decrease Counter" onPress={decreaseCounter} />
     </SafeAreaView>
   );
-}
+};
 
 export default App;
